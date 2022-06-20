19ea7cd12b9499e81c40e55a010d10a61a8826c3
Côme B <cbrocas@cnil.fr>
2022-06-20 14:03:37 +0200

    fichier inutile effacé, renommé test-parse.js en csv-parse.js

--- tests/test-parse.js
+++
@@ -1,8 +0,0 @@
-console.log('running');
-d3.csv("../data.csv", function(data) {
-    for (var i = 0; i < data.length; i++) {
-        console.log("ca marche (pas)")
-        console.log(data[i].nom);
-        console.log(data[i].prix);
-    }
-});
